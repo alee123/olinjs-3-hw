bfbcfe610179457092607c5337140c5b45946a15
Amanda Lee <alee1@alee1-Latitude-e6420.(none)>
2013-02-01 22:32:51 -0500

    fixing mistake

--- models/ingredient.js
+++ models/ingredient.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
-//mongoose.connect(process.env.MONGOLAB_URI || 'localhost');
-mongoose.connect('mongodb://localhost/burgers')
+mongoose.connect(process.env.MONGOLAB_URI || 'localhost');
+//mongoose.connect('mongodb://localhost/burgers')
 
 var ingredientSchema = mongoose.Schema({
   name: String,
